29b6e1298a18d7edf6be7fe7cbb81ffbf1c210cf
iulove1314520 <connorrelana@gmail.com>
2026-03-22 19:29:47 +0800

    1

--- 通用节点过滤.js
+++ 通用节点过滤.js
@@ -4,7 +4,7 @@
  */
 
 const INVALID_NODE_PATTERN =
-  /(?:\W|^)(频道|订阅|套餐|到期|有效|剩余|版本|已用|过期|失联|测试|官方|网址|备用|群|客服|网站|获取|流量|机场|下次|官|系|邮箱|工单|学术|USE|USED|TOTAL|EXPIRE|EMAIL|Traffic)(?:\W|$)/i;
+  /(?:\W|^)(Premium|频道|订阅|套餐|到期|有效|剩余|版本|已用|过期|失联|测试|官方|网址|备用|群|客服|网站|获取|流量|机场|下次|官|系|邮箱|工单|学术|USE|USED|TOTAL|EXPIRE|EMAIL|Traffic)(?:\W|$)/i;
 
 function operator(proxies = [], targetPlatform, context) {
   void targetPlatform;
